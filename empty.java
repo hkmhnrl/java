8a37722c95e59034e1c12bac7d3e0cd4380aa92a
hkmhnrl <hhkmh4283@gmail.com>
2024-11-18 09:37:17 +0700

    Add files via upload

---
+++ empty.java
@@ -0,0 +1,10 @@
+class cek {
+    public static void main(string[] args) {
+        String name1 = "";
+        String name2 = "hallo";
+
+        System.out.println(name1.isEmpty());
+        System.out.println(name2.isEmpty());
+
+    }
+}
